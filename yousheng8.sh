512d6e7426614bd289ad96cfe7dbc4bc4e939d8d
chaoys <chaoys155@gmail.com>
2012-09-05 21:07:40 +0800

    clean code

--- yousheng8.sh
+++ yousheng8.sh
@@ -10,12 +10,10 @@ digits=$5
 for (( i=first_chp;i<=last_chp;i++ )); do
 	curr_chp=`printf "%0${digits}d" $i`
 	url=${url_prefix}down_${book}_${curr_chp}.${html_suffix}
-	echo "URL = $url"
 	wget -nv -O curr.htm $url
 	iconv -f GBK -t UTF-8 -o curr.htm.utf8 curr.htm
 	link=`grep "\.mp3" curr.htm.utf8 | cut -d\" -f2`
-	echo "$link" #>>links
-	wget -nv -nc -c -O ${curr_chp}.mp3 $link
+	wget -nc -c -O ${curr_chp}.mp3 $link
 	#prozilla is much faster than wget
 #	proz $link
 done
